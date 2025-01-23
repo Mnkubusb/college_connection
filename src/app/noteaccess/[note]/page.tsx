948ce5a31ae1b8d00180cc9986b55974c1a5673a
Manik  Chand Sahu <65947443+Mnkubusb@users.noreply.github.com>
2025-01-23 23:11:55 +0530

    overflow

--- src/app/noteaccess/[note]/page.tsx
+++ src/app/noteaccess/[note]/page.tsx
@@ -32,7 +32,7 @@ const NotesListPage = async ( {params}:{params: {note:string}} ) => {
       <div className='relative h-[90vh] sm:overflow-hidden sm:h-full sm:w-full'>
       <Header Name={note.title}></Header>
       <div className='flex relative h-[80vh] sm:h-full sm:w-full'>
-        <div className="md:flex h-full min-h-[80vh] sm:mx-3 px-3 sm:p-6 border-x flex-col lg:col-span-2 bg-background absolute sm:relative sm:w-full w-[100%]">
+        <div className="md:flex h-full min-h-[80vh] sm:mx-3 px-3 sm:p-6 border-x flex-col lg:col-span-2 bg-background absolute sm:relative sm:w-full w-[100%] overflow-auto scroll scroll-smooth">
           <NotesListDisplay items={note.notesList} />
         </div>
       </div>
