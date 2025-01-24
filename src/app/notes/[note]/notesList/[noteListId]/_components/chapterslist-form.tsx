651afe9ad3287ea4fbb1c80ff09a52d58a4c5090
Manik  Chand Sahu <65947443+Mnkubusb@users.noreply.github.com>
2025-01-24 23:23:01 +0530

    deployment error fixed

--- src/app/notes/[note]/notesList/[noteListId]/_components/chapterslist-form.tsx
+++ src/app/notes/[note]/notesList/[noteListId]/_components/chapterslist-form.tsx
@@ -6,13 +6,11 @@ import { useState } from "react";
 import axios from "axios";
 
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { toast } from "react-hot-toast";
 import { Loader2, PlusCircle } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Chapters, NotesList } from "@prisma/client";
-import { Input } from "@/components/ui/input";
 import { ChapterListPage } from "./chapters-list";
 import { EditModal } from "@/components/modals/edit-modal";
 
@@ -46,19 +44,6 @@ export const ChaptersListForm = ({ initialData, noteId, noteListId }: ChaptersLi
 
     const toggleEdit = () => {
         setIsCreating((current) => !current);
-    }
-
-    const { isSubmitting, isValid } = form.formState;
-
-    const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        try {
-            await axios.post(`/api/notes/${noteId}/notesList/${noteListId}/chapters`, values);
-            toast.success("Chapter updated successfully");
-            toggleEdit();
-            router.refresh();
-        } catch (error) {
-            toast.error("Failed to update chapter");
-        }
     }
 
     const onReorder = async (updateData: { id: string, position: number }[]) => {
@@ -96,30 +81,6 @@ export const ChaptersListForm = ({ initialData, noteId, noteListId }: ChaptersLi
                     </Button>
                 </EditModal>
             </div>
-            {/* {isCreating && (
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 mt-4">
-                        <FormField
-                            control={form.control}
-                            name="title"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Title</FormLabel>
-                                    <FormControl>
-                                        <Input disabled={isSubmitting} placeholder="e.g.'Chemistry Notes'" {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <div className="flex items-center gap-x-2">
-                            <Button disabled={!isValid || isSubmitting} type="submit" variant={"secondary"}>
-                                Create
-                            </Button>
-                        </div>
-                    </form> 
-                </Form>
-            )} */}
                 <div className={cn("text-sm mt-2", !initialData.chapters && "text-muted-foreground italic")}>
                     {!initialData.chapters.length && "No Notes"}
                     <ChapterListPage
