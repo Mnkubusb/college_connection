7389ad0734927fdbb692c6ba4d91146218780186
Mnkubusb <manikmnr315@gmail.com>
2025-09-06 13:45:33 +0530

    onboarding added

--- src/components/auth/onboard-form.tsx
+++ src/components/auth/onboard-form.tsx
@@ -91,6 +91,10 @@ const batches = [
   { label: "2022", value: "2022" },
   { label: "2023", value: "2023" },
   { label: "2024", value: "2024" },
+  { label: "2025", value: "2025" },
+  { label: "2026", value: "2026" },
+  { label: "2027", value: "2027" },
+  { label: "2028", value: "2028" },
 ] as const
 
 const skills = [
